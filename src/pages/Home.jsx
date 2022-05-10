1c89f14dc7adc4f16a7ba4b119f0cc681db956bf
saswatsam786 <saswat.sam786@gmail.com>
2022-05-10 14:21:27 +0530

    added the home page and made it responsive using styled-components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,166 +1,132 @@
 import React from "react";
-import RightImage from "../Utilities/RightImage.jpg";
-import Header from "../components/Header";
 import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebaseConfig";
+import "./Home.css";
 import { mobile } from "../Utilities/responsive";
-import { auth, provider } from "../firebaseConfig";
 
 const Home = () => {
-  const signin = async (e) => {
-    e.preventDefault();
-    provider.setCustomParameters({ prompt: "select_account" });
-    await auth
-      .signInWithPopup(provider)
-      .catch(alert)
-      .then(() => {
-        const user = auth.currentUser;
-        console.log(user.phoneNumber);
-      });
-  };
-
+  const [user] = useAuthState(auth);
   return (
     <Container>
-      {/* Home Container */}
-      <HomeCon>
-        {/* inner glass box */}
-        <MainBox>
-          <Header />
-          <MainContent>
-            <MainText>
-              <h1>
-                Gate <span>Keeper</span>
-              </h1>
-              <h2>One of a kind attendence management system</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-                tempora error consequatur veniam soluta cumque ducimus! Soluta
-              </p>
-              <Button onClick={signin}>Login Now</Button>
-            </MainText>
-            <MainImage>
-              <ImageRight src={RightImage} alt="Right" />
-            </MainImage>
-          </MainContent>
-        </MainBox>
-      </HomeCon>
-      <Circle top="4%" right="2%"></Circle>
-      <Circle bottom="4%" left="2%"></Circle>
+      <Dashboard>
+        <User>
+          <Avatar
+            src={user.photoURL}
+            alt={user.displayName}
+            sx={{ width: 70, height: 70 }}
+          />
+          <h2>{user.displayName}</h2>
+        </User>
+        <Links>
+          <Child>
+            <img src="./images/twitch.png" alt="" />
+            <h2>Streams</h2>
+          </Child>
+          <Child>
+            <img src="./images/steam.png" alt="" />
+            <h2>Games</h2>
+          </Child>
+          <Child>
+            <img src="./images/upcoming.png" alt="" />
+            <h2>New</h2>
+          </Child>
+          <Child>
+            <img src="./images/library.png" alt="" />
+            <h2>Library</h2>
+          </Child>
+        </Links>
+        <Pro>
+          <h2>Join pro for free games.</h2>
+        </Pro>
+      </Dashboard>
+      <Details>helo</Details>
     </Container>
   );
 };
 
 export default Home;
 
-const Container = styled.section``;
-
-const HomeCon = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  background: linear-gradient(to right top, #65dfc9, #6cdbeb);
+const Container = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const MainBox = styled.div`
-  background: white;
-  width: 80%;
   min-height: 80vh;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Dashboard = styled.div`
+  flex: 0.4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
   background: linear-gradient(
     to right bottom,
-    rgba(255, 255, 255, 0.7),
-    rgba(255, 255, 255, 0.3)
-  );
-  border-radius: 1rem;
-  backdrop-filter: blur(0.5rem);
-  z-index: 4;
-  ${mobile({ width: "90%" })}
-`;
-
-const MainContent = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 50px;
-  ${mobile({ flexDirection: "column-reverse" })}
-`;
-
-const MainText = styled.div`
-  flex: 1;
-  & > h1 {
-    font-size: 2.5rem;
-    margin-bottom: 10px;
-  }
-  & > h2 {
-    font-size: 2rem;
-    font-weight: 200;
-  }
-
-  & > p {
-    font-size: 1rem;
-    font-weight: 100;
-    margin: 18px 0 40px;
-  }
-
-  &:after {
-    content: "";
-    width: 5px;
-    height: 20%;
-    background: #089d8f;
-    position: absolute;
-    left: 2.4rem;
-    top: 12rem;
-    ${mobile({ bottom: "11rem" })}
-  }
-
-  ${mobile({ marginTop: "10px" })}
-`;
-
-const Button = styled.button`
-  padding: 15px;
-  border-radius: 10px;
-  color: #426696;
-  font-size: 1.2rem;
-  background: linear-gradient(to left top, #65dfc9, #6cdbeb);
-  transition: 0.3s ease-in-out;
-  outline: none;
-  border: none;
-  transition: all 1s ease;
-  cursor: pointer;
-  &:hover {
-    background: linear-gradient(to right bottom, #26a890, #3fe3fb);
-    transition: all 1s ease-in-out;
-  }
-`;
-
-const MainImage = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ImageRight = styled.img`
-  width: 95%;
-  border-radius: 10px;
-`;
-
-const Circle = styled.div`
-  background: white;
-  height: 15rem;
-  width: 15rem;
-  border-radius: 50%;
-  position: absolute;
-  background: linear-gradient(
-    to right bottom,
-    rgba(255, 255, 255, 0.9),
+    rgba(255, 255, 255, 0.5),
     rgba(255, 255, 255, 0.2)
   );
-  z-index: 2;
-  top: ${(props) => props.top};
-  right: ${(props) => props.right};
-  bottom: ${(props) => props.bottom};
-  left: ${(props) => props.left};
+  border-radius: 2rem;
+  padding: 5px 0;
+  ${mobile({ justifyContent: "flex-start", flex: "0.1", padding: "0" })}
 `;
 
-// const Circle = styled.div``;
+const User = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > h2 {
+    color: #426696;
+    font-weight: 600;
+    opacity: 0.8;
+    font-size: 30px;
+    padding: 10px;
+    text-align: center;
+  }
+`;
+
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  height: 100%;
+  ${mobile({ flexDirection: "row" })}
+`;
+
+const Child = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+  transition: color 6s ease-in-out;
+  border-radius: 10px;
+  cursor: pointer;
+  & > h2 {
+    color: #658ec6;
+    font-weight: 500;
+    opacity: 0.8;
+  }
+
+  &:hover {
+    background: linear-gradient(to right top, #65dfc9, #6cdbeb);
+  }
+  ${mobile({ padding: "10px" })}
+`;
+
+const Pro = styled.div`
+  background: linear-gradient(to right top, #65dfc9, #6cdbeb);
+  border-radius: 2rem;
+  color: white;
+  padding: 1rem;
+  position: relative;
+`;
+
+const Details = styled.div`
+  flex: 1;
+  display: flex;
+  margin: 2rem 0rem;
+  padding: 1rem 5rem;
+  align-items: center;
+  flex-direction: column;
+`;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,168 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import RightImage from "../Utilities/RightImage.jpg";
+import Header from "../components/Header";
+import styled from "styled-components";
+import { mobile } from "../Utilities/responsive";
+import { db, auth, provider } from "../firebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) navigate("/home");
+  }, [navigate, user]);
+
+  const signin = async (e) => {
+    e.preventDefault();
+    provider.setCustomParameters({ prompt: "select_account" });
+    await auth
+      .signInWithPopup(provider)
+      .catch(alert)
+      .then(() => {
+        const user = auth.currentUser;
+        let acc = false;
+        db.collection("accounts")
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach(async (doc) => {
+              if (doc.data().email === user.email) {
+                acc = true;
+              }
+            });
+            if (acc === false) createAccount();
+          });
+      });
+  };
+
+  async function createAccount() {
+    const user = auth.currentUser;
+    const createAcc = new Date();
+    await axios.get("http://localhost:8000/createAccount").then((props) => {
+      db.collection("accounts")
+        .add({
+          email: user.email,
+          accid: props.data.id,
+          privatekey: props.data.privatekey,
+          publickey: props.data.publickey,
+          lib: [],
+          accountCreationDate: createAcc.toLocaleDateString(),
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <MainContent>
+        <MainText>
+          <h1>
+            Gate <span>Keeper</span>
+          </h1>
+          <h2>One of a kind attendence management system</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            tempora error consequatur veniam soluta cumque ducimus! Soluta
+          </p>
+          <Button onClick={signin}>Login Now</Button>
+        </MainText>
+        <MainImage>
+          <ImageRight src={RightImage} alt="Right" />
+        </MainImage>
+      </MainContent>
+    </>
+  );
+};
+
+export default Home;
+
+// const MainBox = styled.div`
+//   background: white;
+//   width: 80%;
+//   min-height: 80vh;
+//   background: linear-gradient(
+//     to right bottom,
+//     rgba(255, 255, 255, 0.7),
+//     rgba(255, 255, 255, 0.3)
+//   );
+//   border-radius: 1rem;
+//   backdrop-filter: blur(0.5rem);
+//   z-index: 4;
+//   ${mobile({ width: "90%" })}
+// `;
+
+const MainContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 50px;
+  ${mobile({ flexDirection: "column-reverse" })}
+`;
+
+const MainText = styled.div`
+  flex: 1;
+  & > h1 {
+    font-size: 2.5rem;
+    margin-bottom: 10px;
+  }
+  & > h2 {
+    font-size: 2rem;
+    font-weight: 200;
+  }
+
+  & > p {
+    font-size: 1rem;
+    font-weight: 100;
+    margin: 18px 0 40px;
+  }
+
+  &:after {
+    content: "";
+    width: 5px;
+    height: 20%;
+    background: #089d8f;
+    position: absolute;
+    left: 2.4rem;
+    top: 12rem;
+    ${mobile({ bottom: "11rem" })}
+  }
+
+  ${mobile({ marginTop: "10px" })}
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border-radius: 10px;
+  color: #426696;
+  font-size: 1.2rem;
+  background: linear-gradient(to left top, #65dfc9, #6cdbeb);
+  transition: 0.3s ease-in-out;
+  outline: none;
+  border: none;
+  transition: all 1s ease;
+  cursor: pointer;
+  &:hover {
+    background: linear-gradient(to right bottom, #26a890, #3fe3fb);
+    transition: all 1s ease-in-out;
+  }
+`;
+
+const MainImage = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageRight = styled.img`
+  width: 95%;
+  border-radius: 10px;
+`;
+
+// const Circle = styled.div``;
