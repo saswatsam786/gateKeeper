70fb6cec63f1daa757472830f5d99eb73c6e8cfe
saswatsam786 <saswat.sam786@gmail.com>
2022-05-27 19:09:57 +0530

    added a fact generator for knowledge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Avatar from "@mui/material/Avatar";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -7,9 +7,34 @@ import "./Home.css";
 import { mobile } from "../Utilities/responsive";
 import Video from "../components/Video";
 import Modal from "../components/Modal";
+import Statistics from "../components/Statistics";
+import Hbar from "../components/Hbar";
+import Settings from "../components/Settings";
+import axios from "axios";
 
 const Home = () => {
   const [user] = useAuthState(auth);
+  const [attendence, setAttendence] = useState(true);
+  const [stats, setStats] = useState(false);
+  const [hbar, setHbar] = useState(false);
+  const [settings, setSettings] = useState(false);
+  const [fact, setFact] = useState("Click for random facts");
+
+  async function factGenerator() {
+    setFact("");
+    const result = await axios.get(
+      "https://api.api-ninjas.com/v1/facts?limit=1",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Api-Key": "9YxLHxmkg6g9w/MolF8XeQ==xCWa4ssqaGev9qWx",
+        },
+      }
+    );
+    setFact(result.data[0].fact);
+    console.log(result.data[0].fact);
+  }
+
   return (
     <Container>
       <Modal />
@@ -23,29 +48,86 @@ const Home = () => {
           <h2>{user.displayName}</h2>
         </User>
         <Links>
-          <Child>
-            <img src="./images/twitch.png" alt="" />
-            <h2>Streams</h2>
+          <Child
+            onClick={() => {
+              setStats(false);
+              setHbar(false);
+              setSettings(false);
+              setAttendence(true);
+            }}
+            style={{
+              background:
+                attendence && "linear-gradient(to right top, #65dfc9, #6cdbeb)",
+            }}
+          >
+            <h2>Attendence</h2>
           </Child>
-          <Child>
-            <img src="./images/steam.png" alt="" />
-            <h2>Games</h2>
+          <Child
+            onClick={() => {
+              setAttendence(false);
+              setHbar(false);
+              setSettings(false);
+              setStats(true);
+            }}
+            style={{
+              background:
+                stats && "linear-gradient(to right top, #65dfc9, #6cdbeb)",
+            }}
+          >
+            <h2>Statistics</h2>
           </Child>
-          <Child>
-            <img src="./images/upcoming.png" alt="" />
-            <h2>New</h2>
+          <Child
+            onClick={() => {
+              setAttendence(false);
+              setStats(false);
+              setSettings(false);
+              setHbar(true);
+            }}
+            style={{
+              background:
+                hbar && "linear-gradient(to right top, #65dfc9, #6cdbeb)",
+            }}
+          >
+            <h2>Hbar</h2>
           </Child>
-          <Child>
-            <img src="./images/library.png" alt="" />
-            <h2>Library</h2>
+          <Child
+            onClick={() => {
+              setAttendence(false);
+              setStats(false);
+              setHbar(false);
+              setSettings(true);
+            }}
+          >
+            <h2>Settings</h2>
           </Child>
         </Links>
-        <Pro>
-          <h2>Join pro for free games.</h2>
+        <Pro onClick={factGenerator}>
+          <p
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginBottom: 0,
+            }}
+          >
+            {!fact ? (
+              <lottie-player
+                src="https://assets8.lottiefiles.com/packages/lf20_fl4lnt7z.json"
+                background="transparent"
+                loop
+                autoplay
+                style={{ width: "10%" }}
+              ></lottie-player>
+            ) : (
+              `${fact}`
+            )}
+          </p>
         </Pro>
       </Dashboard>
       <Details>
-        <Video />
+        {attendence && <Video play={attendence} style={{ padding: "10px" }} />}
+        {stats && <Statistics />}
+        {hbar && <Hbar />}
+        {settings && <Settings />}
       </Details>
     </Container>
   );
@@ -129,6 +211,14 @@ const Pro = styled.div`
   color: white;
   padding: 1rem;
   position: relative;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: "100%";
+  &:hover {
+  }
+
   ${mobile({ marginTop: "2px" })}
 `;
 
@@ -136,7 +226,9 @@ const Details = styled.div`
   flex: 1;
   display: flex;
   margin: 2rem 0rem;
-  padding: 1rem 5rem;
   align-items: center;
+  justify-content: center;
   flex-direction: column;
+  width: 100%;
+  position: relative;
 `;
